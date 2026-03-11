52fca0c8466c6f3013bb589e828aca9ea81ae454
matzxcss <matheuslacerda01@proton.me>
2026-03-11 14:21:12 -0300

    fix return

--- services/cinema_hall.py
+++ services/cinema_hall.py
@@ -7,7 +7,7 @@ def get_cinema_halls() -> list[CinemaHall]:
 
 def create_cinema_hall(
     hall_name: str, hall_rows: int, hall_seats_in_row: int
-) -> CinemaHall:
+) -> None:
     CinemaHall.objects.create(
         name=hall_name, rows=hall_rows, seats_in_row=hall_seats_in_row
     )
